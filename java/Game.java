631c6adb810dc949cd3e4aa0f9482b33c885072f
lmoran4 <56325104+lmoran4@users.noreply.github.com>
2019-10-30 16:27:56 -0400

    score implemented

--- java/Game.java
+++ java/Game.java
@@ -2,6 +2,8 @@
 
 //NOTE: be sure to change the package to reflect yours!
 
+package com.example.leila.stateplatebingo;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -10,7 +12,6 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ListAdapter;
 import android.widget.TextView;
@@ -26,10 +27,17 @@ public class Game extends Activity {
 
     private GridLayout gl; // layout for grid
 
-    // gridStr is for future unit tests
-    private String[][] gridStr;
+    // gridStr is for future unit tests/saving/etc
+    private String[][] gridStr;                      // NEED TO SAVE
+
+    private boolean[][] cellCheck;
 
     private TextView[][] cells; // the grid cells
+
+
+    //private String[] stateFileStrs;
+
+    HashMap<String,String> stateFileStrs; //state, str
 
     String[] keys;
     String[] vals;
@@ -37,97 +45,84 @@ public class Game extends Activity {
     ArrayList<String> states;
     ArrayList<String> abbs;
 
-    ArrayList<String> setstates; // states chosen by user
-
+    ArrayList<String> setstates; // states chosen by user           // NEED TO SAVE
     ArrayList<String> shuffleAbs; // shuffled abbreviations
 
-    HashMap<String, String> stateAbbs; // (state,abbreviation)
-    HashMap<String, RowCol> boardhelpr; // (abbreviation, (row,col))
+    HashMap<String, String> stateAbbs; // (state,abbreviation)       // DO NOT NEED TO SAVE
+    HashMap<String, RowCol> boardhelpr; // (abbreviation, (row,col)) // NEED TO SAVE
 
-    ArrayAdapter<String> spindapter;
-    ArrayAdapter<String> adapter;
+    //private Button selector;
 
-    //Spinner spin; // dropdown / dialog of selectable states
+    private int score;         // NEED TO SAVE
 
-    Button selector;
+    private TextView scoreView;
 
-    int score;
+    boolean[] block1rows;
+    boolean[] block2rows;
+    boolean[] block3rows;
+    boolean[] block4rows;
 
-    //private AlertDialog.Builder statePicker;
-    //private AlertDialog.Builder builderC;
-    //AlertDialog actions;
+    boolean[] col1;
+    boolean[] col2;
+    boolean[] col3;
+
+    boolean[] allCols;
+
+    boolean[] blocks;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game); // links Game.java to activity_game.xml
 
-        gl = (GridLayout) findViewById(R.id.board);
+        this.gl = (GridLayout) findViewById(R.id.board);
 
-        gridStr = new String[20][3];
+        this.gridStr = new String[20][3];
         stateAbbs = new HashMap<>();
         boardhelpr = new HashMap<>();
+        stateFileStrs = new HashMap<>();
 
         states = new ArrayList<>();
         abbs = new ArrayList<>();
         setstates = new ArrayList<>();
         shuffleAbs = new ArrayList<>();
 
-        selector = (Button) findViewById(R.id.selectstatebutton);
+        //this.selector = (Button) findViewById(R.id.selectstatebutton);
 
-        //spin = (Spinner) findViewById(R.id.spinners);
+        this.scoreView = (TextView) findViewById(R.id.scoreview);
 
         setupStates();
 
         buildGrid();
 
-
-        /*statePicker = new AlertDialog.Builder(this);
-        statePicker.setTitle("Select State/Province");
-
-        statePicker.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                //
-
-            }
-        });
-
-        statePicker.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                //
-
-            }
-        });
-
-        actions = statePicker.create();*/
+        buildBlocks();
     }
 
 
     public void setupStates(){
 
-       keys = new String[]{"Alabama", "Alaska", "Arizona", "Arkansas",
-               "California", "Colorado", "Connecticut", "Delaware",
-               "Florida", "Georgia","Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
-               "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan",
-               "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada",
-               "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota",
-               "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island",
-               "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah",
-               "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming",
-               "Ontario","Quebec", "Nova Scotia", "New Brunswick", "Manitoba", "British Columbia",
-               "Prince Edward Island", "Saskatchewan", "Alberta", "Newfoundland & Labrador"};
+        keys = new String[]{"Alabama", "Alaska", "Arizona", "Arkansas",
+                "California", "Colorado", "Connecticut", "Delaware",
+                "Florida", "Georgia","Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+                "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan",
+                "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada",
+                "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota",
+                "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island",
+                "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah",
+                "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming",
+                "Ontario","Quebec", "Nova Scotia", "New Brunswick", "Manitoba", "British Columbia",
+                "Prince Edward Island", "Saskatchewan", "Alberta", "Newfoundland & Labrador"};
 
-       vals = new String[]{"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE",
-               "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS",
-               "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO",
-               "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND",
-               "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX",
-               "UT", "VT", "VA", "WA", "WV", "WI", "WY",
-               "ON","QC","NS","NB","MB","BC","PE","SK","AB","NL"};
+        vals = new String[]{"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE",
+                "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS",
+                "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO",
+                "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND",
+                "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX",
+                "UT", "VT", "VA", "WA", "WV", "WI", "WY",
+                "ON","QC","NS","NB","MB","BC","PE","SK","AB","NL"};
 
-        //states = (ArrayList<String>) Arrays.asList(keys);
-       // abbs = (ArrayList<String>) Arrays.asList(vals);
+
 
         // sets up state/abbreviation k/v pairs
         for(int i =0; i<keys.length; i++){
@@ -148,14 +143,51 @@ public class Game extends Activity {
 
         score = 0;
 
+        scoreView.setText(String.valueOf(score));
+
         // randomizes the list of state abbreviations
         Collections.shuffle(shuffleAbs, new Random());
     }
 
 
+    public void loadBlocks(){
+
+    }
+
+    public void buildBlocks(){
+
+        allCols = new boolean[] {false,false,false};
+        blocks = new boolean[] {false,false,false,false};
+
+        col1 = new boolean[20];
+        col2 = new boolean[20];
+        col3 = new boolean[20];
+
+        for(int i = 0; i<20; i++){
+            col1[i] = false;
+            col2[i] = false;
+            col3[i] = false;
+        }
+
+        block1rows = new boolean[5];
+        block2rows = new boolean[5];
+        block3rows = new boolean[5];
+        block4rows = new boolean[5];
+
+
+        for(int i = 0; i<5; i++) {
+            block1rows[i] = false;
+            block2rows[i] = false;
+            block3rows[i] = false;
+            block4rows[i] = false;
+        }
+    }
+
+    // FIX FOR LOADING
     public void buildGrid(){
 
         cells = new TextView[20][3];
+        cellCheck = new boolean[20][3];
 
         // scales cells with screen size
         final float scale = this.getResources().getDisplayMetrics().density;
@@ -170,12 +202,16 @@ public class Game extends Activity {
                 cells[i][j].setBackgroundResource(R.drawable.gridbutton);
                 cells[i][j].setForeground(getDrawable(R.drawable.gridbutton));
 
-              //  cells[i][j].setText("");
+                String stateA = shuffleAbs.get(counter);
+
                 cells[i][j].setText(shuffleAbs.get(counter));
 
-                gridStr[i][j] = shuffleAbs.get(counter);
+                gridStr[i][j] = stateA;
 
-                RowCol thingy = new RowCol(i,j);
+                cellCheck[i][j] = false;
+
+                RowCol thingy = new RowCol(i,j,stateName,stateA);
+                thingy.setMarked(false);
 
                 boardhelpr.put(shuffleAbs.get(counter), thingy); // adds (stateAbbr, (row,col)) pair to hashmap
 
@@ -189,36 +225,38 @@ public class Game extends Activity {
         }
     }
 
-    //TODO:
-    //public int scoreCalculator(...){
-    // ....
-    // }
-    public void bingoCheckr(){
-    // ...
-
-        // state: +1
-        // row:   +3 bonus
-        // col: + 20 bonus
-
-        //full block/5 rows full: +10
-        //block strip: 5 vertical cells full: +5
-
-        /*
-
-        check:
-
-        rows     cols
-        0-4       0-2
-        5-9     0-2
-        10-14   0-2
-        15-19   0-2
 
 
-        block 1:
 
-        rows 0-4
+    public void saveStrs(){
 
-         */
+
+
+
+
+    }
+
+
+
+
+
+    public void bingoCheckr(String stateStr){
+
+        String stateAb = stateAbbs.get(stateStr);
+
+        RowCol loc = boardhelpr.get(stateAb);
+
+        int row = loc.getRow();
+        int col = loc.getCol();
+        //String name = loc.getName();
+
+        colCheckr(row,col);
+
+        stripCheckr(row,col);
+
+        rowCheckr(row,col);
+
+        scoreView.setText(String.valueOf(score));
 
 
     }
@@ -231,21 +269,375 @@ public class Game extends Activity {
 
         String stateAb = stateAbbs.get(stateStr);
 
+        boardhelpr.get(stateAb).setMarked(true);
+
         RowCol loc = boardhelpr.get(stateAb);
 
         int row = loc.getRow();
         int col = loc.getCol();
 
-        cells[row][col].setBackgroundResource(R.drawable.marker);
+
+        cellCheck[row][col] = true;
+
+        if(col == 0){
+            col1[row] = true;
+        }
+        if (col==1){
+            col2[row] = true;
+        }
+        if(col==2){
+            col3[row] = true;
+        }
+
+
+        if(row<5){
+
+            cells[row][col].setBackgroundResource(R.drawable.marker); //red
+
+        }
+
+
+        else if(row < 10 && row >=5) {
+
+            cells[row][col].setBackgroundResource(R.drawable.marker2); //blue
+
+        }
+
+
+        else if(row < 15 && row >= 10) {
+
+            cells[row][col].setBackgroundResource(R.drawable.marker3); //green/yellow
+
+
+        }
+
+
+        else  if(row < 20 && row >= 15) {
+
+            cells[row][col].setBackgroundResource(R.drawable.marker4); // orange
+
+        }
+        else {
+            cells[row][col].setBackgroundResource(R.drawable.errormark); //purple
+        }
+
+        bingoCheckr(stateStr);
 
     }
+
+
+
+
+
+    public void colCheckr(int row, int col){
+
+        boolean colChanged = false;
+
+        if(col == 0){
+
+            col1[row] = true;
+
+            boolean allTrue=true;
+            for(boolean b : col1){
+                if (!b){
+                    allTrue=false;
+                    break;
+                }
+            }
+
+            if(allTrue){
+                allCols[col] = true;
+                colChanged =true;
+                score=score+20;
+                //CHECK ALL COLS
+            }
+
+        }
+
+        if (col==1){
+
+            col2[row] = true;
+
+            boolean allTrue=true;
+            for(boolean b : col2){
+                if (!b){
+                    allTrue=false;
+                    break;
+                }
+            }
+
+            if(allTrue){
+                allCols[col] = true;
+                colChanged =true;
+                score=score+20;
+                //CHECK ALL COLS
+            }
+        }
+
+
+        if(col==2){
+
+            col3[row] = true;
+
+            boolean allTrue=true;
+            for(boolean b : col3){
+                if (!b){
+                    allTrue=false;
+                    break;
+                }
+            }
+
+            if(allTrue){
+                allCols[col] = true;
+                colChanged =true;
+                score=score+20;
+                //CHECK ALL COLS
+            }
+
+        }
+
+
+        // board is complete!
+        if(allCols[0] && allCols[1] && allCols[2]) {
+
+            score = score + 60;
+        }
+
+
+    }
+
+
+
+
+    // checks the strip the state is in
+    public void stripCheckr(int row, int col) {
+
+
+        if (row < 5) {
+
+            boolean stripTrue = true;
+
+            for (int i = 0; i < 5; i++) {
+
+                if (!cellCheck[i][col]) {
+                    stripTrue = false;
+                    break;
+                }
+
+            }
+
+            if (stripTrue) {
+                score = score + 5;
+            }
+
+        }
+
+
+
+
+        if(row < 10 && row >=5) {
+
+            boolean stripTrue = true;
+
+            for (int i = 5; i < 10; i++) {
+
+                if (!cellCheck[i][col]) {
+                    stripTrue = false;
+                    break;
+                }
+
+            }
+
+            if (stripTrue) {
+                score = score + 5;
+            }
+        }
+
+
+        if(row < 15 && row >= 10) {
+
+            boolean stripTrue = true;
+
+            for (int i = 10; i < 15; i++) {
+
+                if (!cellCheck[i][col]) {
+                    stripTrue = false;
+                    break;
+                }
+
+            }
+
+            if (stripTrue) {
+                score = score + 5;
+            }
+        }
+
+
+        if(row < 20 && row >= 15) {
+
+            boolean stripTrue = true;
+
+            for (int i = 15; i < 20; i++) {
+
+                if (!cellCheck[i][col]) {
+                    stripTrue = false;
+                    break;
+                }
+
+            }
+
+            if (stripTrue) {
+                score = score + 5;
+            }
+        }
+
+    }
+
+
+
+    // checks all rows and blocks
+    public void rowCheckr(int row, int col){
+
+        if(row < 5) {
+
+            boolean allTrue = true;
+
+            for (int j = 0; j < 3; j++) {
+
+                if (!cellCheck[row][j]) {
+                    allTrue = false;
+                    break;
+                }
+
+            }
+
+            if (allTrue) {
+                score = score + 3;
+                block1rows[row] = true;
+
+                boolean allRows=true;
+                for(boolean b : block1rows){
+                    if(!b){
+                        allRows=false;
+                        break;
+                    }
+                }
+
+                if(allRows){
+                    score = score + 15;
+                }
+            }
+        }
+
+
+
+        if(row < 10 && row >=5) {
+
+            boolean allTrue = true;
+
+            for (int j = 0; j < 3; j++) {
+
+                if (!cellCheck[row][j]) {
+                    allTrue = false;
+                    break;
+                }
+
+            }
+
+            if (allTrue) {
+                score = score + 3;
+                block2rows[row-5] = true;
+
+
+                boolean allRows=true;
+                for(boolean b : block2rows){
+                    if(!b){
+                        allRows=false;
+                        break;
+                    }
+                }
+
+                if(allRows){
+                    score = score + 15;
+                }
+            }
+        }
+
+
+        if(row < 15 && row >= 10) {
+
+            boolean allTrue = true;
+
+            for (int j = 0; j < 3; j++) {
+
+                if (!cellCheck[row][j]) {
+                    allTrue = false;
+                    break;
+                }
+
+            }
+
+            if (allTrue) {
+                score = score + 3;
+                block3rows[row-10] = true;
+
+
+                boolean allRows=true;
+                for(boolean b : block3rows){
+                    if(!b){
+                        allRows=false;
+                        break;
+                    }
+                }
+
+                if(allRows){
+                    score = score + 15;
+                }
+            }
+        }
+
+
+        if(row < 20 && row >= 15) {
+
+            boolean allTrue = true;
+
+            for (int j = 0; j < 3; j++) {
+
+                if (!cellCheck[row][j]) {
+                    allTrue = false;
+                    break;
+                }
+
+            }
+
+            if (allTrue) {
+                score = score + 3;
+                block4rows[row-15] = true;
+
+
+                boolean allRows=true;
+                for(boolean b : block4rows){
+                    if(!b){
+                        allRows=false;
+                        break;
+                    }
+                }
+
+                if(allRows){
+                    score = score + 15;
+                }
+            }
+        }
+
+    }
+
 
     // TODO: WIP
     // method that performs actions after user selects state
     public void selectState(View v) { //public void selectState(String stateStr)
 
         //TODO:
-        // pop state from list of available states, rebuild spinner adapter
+        // pop state from list of available states, rebuild adapter
         // add state/abbr to lists of selected states/abbrs
 
         // score + 1
@@ -313,6 +705,23 @@ public class Game extends Activity {
 
     }
 
+    //TODO
+    public void saveGame(){
+
+        // CATCH EXCEPTION: when there isn't enough space,
+        // toast: sum ting wong with saving bro
+
+
+    }
+
+    //TODO
+    public void loadGame(){
+
+        // IF THERE'S A CONTINUE, load it up!
+        // If not, or something weird happens, catch exception, toast, and start new game.
+
+    }
+
 
 
     // onClick for "Menu" button; goes to menu
@@ -332,9 +741,22 @@ public class Game extends Activity {
         int row;
         int col;
 
-        public RowCol(int row, int col){
+        String name;
+        String abbr;
+        boolean marked;
+
+        //boolean isMarked
+
+        public RowCol(int row, int col, String name, String abbr){
             this.row=row;
             this.col=col;
+            this.name=name;
+            this.abbr=abbr;
+            this.marked=false;
+        }
+
+        public String getName() {
+            return this.name;
         }
 
         public int getCol() {
@@ -343,6 +765,25 @@ public class Game extends Activity {
 
         public int getRow() {
             return this.row;
+        }
+
+        public void setMarked(boolean marked) {
+            this.marked=marked;
+        }
+
+        public boolean isMarked(){
+            return this.marked;
+        }
+
+        @Override
+        public String toString() {
+
+            String thing = this.name + " " + this.abbr + " " + this.row + " " + this.col + " " + this.marked;
+
+            // split by whitespace. NAME, ABBR, ROW, COL, MARKED
+            // parse...
+
+            return thing;
         }
     }
 
